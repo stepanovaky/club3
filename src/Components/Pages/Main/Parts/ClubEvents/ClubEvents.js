0c0f57ed9d131aa465d222894047b8c75e9764e8
Katerina <psyckalla.gmail.com>
2020-12-14 17:32:15 -0500

    fixing errors

--- src/Components/Pages/Main/Parts/ClubEvents/ClubEvents.js
+++ src/Components/Pages/Main/Parts/ClubEvents/ClubEvents.js
@@ -1,30 +1,64 @@
 import React from "react";
 import { dummyEvents } from "../../../../helpers/dummy-events";
+import moment from "moment";
 
 function ClubEvents() {
   //when server is built, there should be an API call
   //to the server to create the events list here
 
-  const listEvents = dummyEvents.map((one, index) => {
-    return (
-      <div className="events-div">
-        <div key={index} className="events-list-item">
-          <div className="frame">
-            <div className="events-frame-content">
-              <div className="events-text">
-                <div className="events-text-top">
-                  <h2 className="event-title">{one.eventName}</h2>
-                  <h5 className="event-date-time">
-                    {one.eventDate}, {one.eventTime}
-                  </h5>
-                  <p>{one.eventDesc}</p>
+  const formattedDate = (date) => {
+    moment(date).format("MMMM Do ddd");
+  };
+
+  const formattedTime = (time) => {
+    moment(time).format("h:mA");
+  };
+
+  const displayEvent = (one, index) => {
+    if (one.mainPage === true) {
+      return (
+        <div className="events-div">
+          <div key={index} className="events-list-item">
+            <div className="frame">
+              <div className="events-frame-content">
+                <div className="events-text">
+                  <div className="events-text-top">
+                    <h2 className="event-title">{one.name}</h2>
+                    <h5 className="event-date-time">
+                      {one.date}, {one.locationRollCallAndInspection}
+                    </h5>
+                    <p>{one.desc}</p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return null;
+    }
+  };
+
+  const listEvents = dummyEvents.map((one, index) => {
+    return displayEvent(one, index);
+    // <div className="events-div">
+    //   <div key={index} className="events-list-item">
+    //     <div className="frame">
+    //       <div className="events-frame-content">
+    //         <div className="events-text">
+    //           <div className="events-text-top">
+    //             <h2 className="event-title">{one.name}</h2>
+    //             <h5 className="event-date-time">
+    //               {formattedDate(one.date)}, {formattedTime(one.locationRollCallAndInspection)}
+    //             </h5>
+    //             <p>{one.eventDesc}</p>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   });
 
   return (
