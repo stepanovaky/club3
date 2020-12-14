413ae4a7f74e613c6ab040041b52d9e67d9b3e6b
Katerina <psyckalla.gmail.com>
2020-12-14 14:24:07 -0500

    updated about section

--- src/Components/Pages/About/About.js
+++ src/Components/Pages/About/About.js
@@ -11,23 +11,28 @@ function About() {
             </div>
             <div className="about-content">
               <p>
-                Sed nisl velit, varius a retrum eu, varius tincidunt metus. Sed
-                tincidunt aliquam nibh at pulvinar. Sed nisl velit, varius a
-                retrum eu, varius tincidunt metus. Sed tincidunt aliquam nibh at
-                pulvinar. Sed nisl velit, varius a retrum eu, varius tincidunt
-                metus. Sed tincidunt aliquam nibh at pulvinar. Sed nisl velit,
-                varius a retrum eu, varius tincidunt metus. Sed tincidunt
-                aliquam nibh at pulvinar. Sed nisl velit, varius a retrum eu,
-                varius tincidunt metus. Sed tincidunt aliquam nibh at pulvinar.
-                Sed nisl velit, varius a retrum eu, varius tincidunt metus.
+                The Central Florida Sighthound Racing Club was formed in 2015 by
+                Ron and Cheryl Bigford. Pat Moran was the first President. Ron
+                wanted an additional format for his whippets to run and perform
+                that was not judged. The first format the Club raced was the
+                Straight WRA (Whippet Racing Association) format of 200yrds. The
+                LGRA (Large Gazehound Racing Association) format was added
+                later. The Club raced at the Sanctuary for the first couple of
+                years and later hosted events at the Florida Horse Park.
               </p>
               <p>
-                Sed nisl velit, varius a retrum eu, varius tincidunt metus. Sed
-                tincidunt aliquam nibh at pulvinar. Sed nisl velit, varius a
-                retrum eu, varius tincidunt metus. Sed tincidunt aliquam nibh at
-                pulvinar. Sed nisl velit, varius a retrum eu, varius tincidunt
-                metus. Sed tincidunt aliquam nibh at pulvinar. Sed nisl velit,
-                varius a retrum eu, varius tincidunt metus.
+                The NOTRA (National Oval Track Racing Association) format was
+                added with the new venue. In 2018 two sand tracks were built at
+                the FHP for racing on sand. Sand is considered the best surface
+                for dogs to race on since there are less injuries. In late 2018
+                the Florida Amendment to end Greyhounds racing for betting
+                purposes led to some confusion and the later part 2019 the Club
+                was looking for another venue. By the beginning of 2020
+                Greyhound racing ended and the FHP allowed the Club to resume a
+                smaller scale program. The Club is always looking to expand the
+                racing formats and new formats will be added with the idea of
+                expanding the entries for events to a broader group of eligible
+                racers.
               </p>
             </div>
           </div>
