39637fceee83b4d93c1fa438e0815b337bb15d87
Katerina <psyckalla.gmail.com>
2020-12-20 14:43:12 -0500

    fixed even more errors

---
+++ src/Components/Pages/Registry/Parts/playgroundNestedFieldArray.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { useFieldArray } from "react-hook-form";
+
+export default ({ nestIndex, control, register }) => {
+  const { fields, remove, append } = useFieldArray({
+    control,
+    name: `dogOwner[${nestIndex}].nestedArray`,
+  });
+
+  return (
+    <div>
+      {fields.map((item, k) => {
+        return (
+          <div key={item.id} style={{ marginLeft: 20 }}>
+            <label>Secondary Owner:</label>
+            <input
+              type="text"
+              placeholder="First name"
+              name={`dogOwner[${nestIndex}].secondary[${k}].firstName`}
+              ref={register({ required: true, maxLength: 80 })}
+            />
+            <input
+              type="text"
+              placeholder="Last name"
+              name={`dogOwner[${nestIndex}].secondary[${k}].lastName`}
+              ref={register({ required: true, maxLength: 100 })}
+            />
+            <input
+              type="text"
+              placeholder="Email"
+              name={`dogOwner[${nestIndex}].secondary[${k}].email`}
+              ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+            />
+            <button type="button" onClick={() => remove(k)}>
+              Delete Secondary Owner
+            </button>
+          </div>
+        );
+      })}
+
+      <button
+        type="button"
+        onClick={() =>
+          append({
+            field1: "field1",
+            field2: "field2",
+          })
+        }
+      >
+        Add Secondary Owner
+      </button>
+
+      <hr />
+    </div>
+  );
+};
