964bfbd06ffa1019dcbccbd77437100aa69a0f3e
Katerina <psyckalla.gmail.com>
2021-01-09 02:09:10 -0500

    adjusted pdf size

--- src/Components/Pages/EventPage/DisplayImg.js
+++ src/Components/Pages/EventPage/DisplayImg.js
@@ -7,7 +7,10 @@ function DisplayImg(props) {
       {/* <img src={props.imgUrl} alt="event premium" /> */}
       <iframe
         src="https://docs.google.com/gview?url=https://firebasestorage.googleapis.com/v0/b/cfs-racing-club.appspot.com/o/Event%20V2%20Saturday.docx.pdf?alt=media&token=029b8367-dac5-4525-b5f6-70d9c7174ce3&embedded=true"
-        style={{ width: "600px", height: "500px" }}
+        style={{
+          width: "100%",
+          "min-height": "500px",
+        }}
         frameborder="0"
       ></iframe>
     </div>
